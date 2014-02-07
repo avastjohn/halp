078f81eecdb22c695467f7c05b4101296e13dc1f
Ava St. John <avasaintjohn@gmail.com>
2014-02-07 12:12:16 -0800

    about to break everything with get json

--- static/js/helpRequest.js
+++ static/js/helpRequest.js
@@ -23,6 +23,13 @@ context.fillText("HALP!", 50, 100);
 
 $(".delete-button").click(function() {
     $( this ).parent().slideUp();
+
+    // use this info as a guide
+
+    // $.getJSON("/index", function(data) {
+    //     newLevel = new Level(data.level_map, data.level_petStart, data.level_treatPos);
+    //     pet.updateLevel(newLevel);
+    });
 });
 
 var hoverOn = function() {
